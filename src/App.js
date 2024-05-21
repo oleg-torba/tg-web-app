08ee0bc327b4f087e96730f499cf5c5f4a6ad491
oleg-torba <100319055+oleg-torba@users.noreply.github.com>
2024-05-21 21:50:03 +0300

    upd

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
 import "./App.css";
+const TG = window.Telegram.WebApp;
 
 function App() {
-  return <div className="App"></div>;
+  useEffect(() => {});
+  const onClose = () => {
+    TG.close();
+  };
+  return (
+    <div className="App">
+      <button onClick={onClose}>Закрити</button>
+    </div>
+  );
 }
 
 export default App;
